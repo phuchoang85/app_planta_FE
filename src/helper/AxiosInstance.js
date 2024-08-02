8dac3805d308ca3211bd1bd04c37f0f53e39f5a9
phuchoang85 <114634047+phuchoang85@users.noreply.github.com>
2024-08-02 15:48:25 +0700

    update ip

--- src/helper/AxiosInstance.js
+++ src/helper/AxiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AxiosInstance = (contentType = 'application/json') => {
     const axiosInstance = axios.create({
-        baseURL: 'http://192.168.1.18:6868/'
+        baseURL: 'http://quockhanh020924.id.vn:6868/'
     });
 
     axiosInstance.interceptors.request.use(
