3435a262151884f0109f805b2f1143fce180ef6a
Phạm Ngọc Quốc Khánh <phamngocquockhanh2004@gmail.com>
2024-08-05 16:03:27 +0700

    fix merge, format document

--- src/helper/AxiosInstance.js
+++ src/helper/AxiosInstance.js
@@ -2,34 +2,30 @@ import axios from 'axios';
 // import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const AxiosInstance = (contentType = 'application/json') => {
-    const axiosInstance = axios.create({
-<<<<<<< HEAD
-        baseURL: 'http://quockhanh020924.id.vn:6868/'
-=======
-        // baseURL: 'http://192.168.10.48:6868/'
-        baseURL: 'https://pdp201.quockhanh020924.id.vn/api/'
->>>>>>> 253f179ab4441a5df34a89b5599c0ddb6682ec59
-    });
+  const axiosInstance = axios.create({
+    // baseURL: 'http://192.168.10.48:6868/'
+    baseURL: 'https://pdp201.quockhanh020924.id.vn/api/',
+  });
 
-    axiosInstance.interceptors.request.use(
-        async (config) => {
-            // const token = await AsyncStorage.getItem('token');
-            const token = '';
-            config.headers = {
-                'Authorization': `Bearer ${token}`,
-                'Accept': 'application/json',
-                'Content-Type': contentType
-            }
-            return config;
-        },
-        err => Promise.reject(err)
-    );
+  axiosInstance.interceptors.request.use(
+    async config => {
+      // const token = await AsyncStorage.getItem('token');
+      const token = '';
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+        'Content-Type': contentType,
+      };
+      return config;
+    },
+    err => Promise.reject(err),
+  );
 
-    axiosInstance.interceptors.response.use(
-        res => res.data,
-        err => Promise.reject(err)
-    );
-    return axiosInstance;
+  axiosInstance.interceptors.response.use(
+    res => res.data,
+    err => Promise.reject(err),
+  );
+  return axiosInstance;
 };
 
 export default AxiosInstance;
--- src/redux/reducer/ReducerConfirmOrder.js
+++ src/redux/reducer/ReducerConfirmOrder.js
@@ -1,28 +1,24 @@
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import {createAction, createReducer} from '@reduxjs/toolkit';
 
 const initialState = {
-    list: [],
-    totalPrice: 0,
-}
+  list: [],
+  totalPrice: 0,
+};
 
-const setConfirmOrder  = createAction('setConfirmOrder ')
-const setTotalPrice = createAction('setTotalPrice')
+const setConfirmOrder = createAction('setConfirmOrder ');
+const setTotalPrice = createAction('setTotalPrice');
 
-const confirmReducer = createReducer(
-    initialState,
-    (builder) => {
-        builder
-            .addCase(setConfirmOrder , (state, action) => {
-                state.list = action.payload
-            })
-            .addCase(setTotalPrice, (state, action) => {
-                state.totalPrice = action.payload
-            })
-            .addDefaultCase((state) =>{
-                return state
-            })
-            
-    }
-)
+const confirmReducer = createReducer(initialState, builder => {
+  builder
+    .addCase(setConfirmOrder, (state, action) => {
+      state.list = action.payload;
+    })
+    .addCase(setTotalPrice, (state, action) => {
+      state.totalPrice = action.payload;
+    })
+    .addDefaultCase(state => {
+      return state;
+    });
+});
 
-export { confirmReducer, setConfirmOrder,setTotalPrice  }
+export {confirmReducer, setConfirmOrder, setTotalPrice};
